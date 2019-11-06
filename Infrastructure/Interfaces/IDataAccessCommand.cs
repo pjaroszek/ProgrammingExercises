4e4d77177705b1e259d2d68d3bf8f4b9d5e4d41a
Paweł Jaroszek <pawel.jaroszek@digit-al.pl>
2019-11-06 09:06:38 +0100

    add command interface anbd order model class

---
+++ Infrastructure/Interfaces/IDataAccessCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace Jaroszek.ProofOfConcept.Infrastructure.Interfaces
+{
+    public interface IDataAccessCommand
+    {
+        void AddNew(OrderDto order);
+        void Update(OrderDto order);
+        void DeleteProdactByProdactCode(string prodactCode);
+    }
+}
---
+++ Infrastructure/Models/OrderDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Jaroszek.ProofOfConcept.Infrastructure.Models
+{
+    using System.Collections.Generic;
+
+    public class OrderDto
+    {
+        public string ContractorName { get; set; }
+        public string Description { get; set; }
+        private List<OrderItemsDto> OrderItemsesDto { get; set; }
+    }
+}
