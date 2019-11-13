a87a0686214ca27ddda9032d1daf9655d22cfc21
Paweł Jaroszek <pawel.jaroszek@digit-al.pl>
2019-11-13 20:12:51 +0100

    use the seq log in Startup class

--- ProgrammingExercises/Startup.cs
+++ ProgrammingExercises/Startup.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace ProgrammingExercises
 {
@@ -24,11 +20,11 @@ namespace ProgrammingExercises
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddLogging(logingBuilder =>{logingBuilder.AddSeq()})
+            services.AddLogging(logingBuilder => { logingBuilder.AddSeq(); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
@@ -40,6 +36,7 @@ namespace ProgrammingExercises
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            logger.LogDebug("Test debug log message");
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
