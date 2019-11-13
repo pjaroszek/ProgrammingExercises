a6efb27446643cb9e7abd424fe2266f415d2a109
Paweł Jaroszek <pawel.jaroszek@digit-al.pl>
2019-11-13 20:08:45 +0100

    add logger seq to project and add in the Startup class

--- ProgrammingExercises/Startup.cs
+++ ProgrammingExercises/Startup.cs
@@ -24,6 +24,7 @@ namespace ProgrammingExercises
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddLogging(logingBuilder =>{logingBuilder.AddSeq()})
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
